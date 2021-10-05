ce47f30a2c8fcb84f9d3d3667346cc26cd910e48
DavidMilai <davidmilai67@gmail.com>
2021-10-05 11:52:31 +0300

    main.ts and packages

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var http = require('http');
+const cors = require('cors');
+const app = express();
+const port = process.env.port || 5000;
+var server = http.createServer(app);
+var io = require('socket.io')(server, {
+  cors: {
+    origin: "*"
+  }
+});
+app.use(express.json());
+app.use(cors());
+
+io.on("connection",(socket)=>{
+    console.log("connected");
+});
+
+server.listen(port, () => {
+  console.log('server started');
+});
