22459c1e83bb2c4f74d06b3577ad668de59f3aef
DavidMilai <davidmilai67@gmail.com>
2021-10-05 12:58:03 +0300

    cors change

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.port || 5000;
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 app.use(express.json());
-app.use(cors());
 
 io.on("connection",(socket)=>{
     console.log("connected");
