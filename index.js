b8531c2a50f20966178bb0e6b0d34367db8087d6
DavidMilai <davidmilai67@gmail.com>
2021-10-05 14:53:25 +0300

    receiving messag from android app

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ var server = http.createServer(app);
 var io = require('socket.io')(server);
 app.use(express.json());
 
-io.on("connection",(socket)=>{
-    console.log("connected");
+io.on('connection', (socket) => {
+  console.log('connected');
+  console.log(socket.id, 'has logged in');
+  socket.on('/test', (msg) => {
+    console.log(msg);
+  });
 });
 
 server.listen(port, "0.0.0.0", () => {
   console.log('server started');
+
 });
