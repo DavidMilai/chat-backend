361fe7aa51a0655786a944931dec3ae0006040d8
DavidMilai <davidmilai67@gmail.com>
2021-10-06 11:31:12 +0300

    send and receive mesages

--- index.js
+++ index.js
@@ -2,20 +2,32 @@ const express = require('express');
 var http = require('http');
 const cors = require('cors');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 app.use(express.json());
 
+var clients ={};
+
 io.on('connection', (socket) => {
-  console.log('connected');
+  console.log('connected')
   console.log(socket.id, 'has logged in');
-  socket.on('/test', (msg) => {
-    console.log(msg);
+
+  socket.on('signin', (id) => {
+    clients[id]= socket;
+    console.log(clients);
+  });
+
+  socket.on("message",(msg)=>{
+    console.log(msg)
+    let targetId = msg.sourceId;
+    if(clients[targetId])
+    clients[targetId].emit("message", msg);
   });
 });
 
-server.listen(port, "0.0.0.0", () => {
-  console.log('server started');
 
+
+server.listen(port, "0.0.0.0", () => {
+  console.log('server started')
 });
